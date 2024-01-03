e4a3da2d16fa184869034d078b2266823dd885a9
jyodai <tdkdtk5011@gmail.com>
2024-01-03 17:32:03 +0900

    * toISOStringだとUTCになってしまう

--- src/utils/date.tsx
+++ src/utils/date.tsx
@@ -4,6 +4,11 @@
  * 日付を YYYY-MM-DDTHH:mm 形式の文字列に変換します。
  */
 export const formatDateToDateTimeLocal = (date : Date): string => {
-    const newDate = new Date(date);
-    return newDate.toISOString().slice(0, 16);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${year}-${month}-${day}T${hours}:${minutes}`;
 };
